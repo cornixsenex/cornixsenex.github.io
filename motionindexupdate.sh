a61ba488d0933e59003177e914f59bffcd404531
cornixsenex <phallusferrus@toke.com>
2022-12-02 11:53:06 -0800

    11:53

--- motionindexupdate.sh
+++ motionindexupdate.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 Date="$(date +%y%m%d)"
+LastVidCount=$(< /home/qwe/code/cornixsenex.github.io/vidcount.txt)
+NewVidCount=$(ls -lrht /home/qwe/Media/Videos/motion/$Date | wc | awk 'NR==1{print $1}')
+
+if [ $LastVidCount == $NewVidCount ]
+then
+	echo "Vid count has NOT changed"
+else
+	echo "Vid count changed"
+fi
 
 cd /home/qwe/Media/Videos/motion
 ls -lrht $Date > /home/qwe/code/cornixsenex.github.io/index.html
